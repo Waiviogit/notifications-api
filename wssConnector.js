ddc35442bb85671df19ab10c4a9bfab1b7ff376c
olegtsigulov <olegcigulev@gmail.com>
2020-04-16 17:09:26 +0300

    change ws logic

--- wssConnector.js
+++ wssConnector.js
@@ -39,12 +39,23 @@ const unSubscribe = (call, result, ws) => {
   });
 };
 
+const getNotifications = async (call, ws) => {
+  try {
+    const res = await redisNotifyClient.lrangeAsync(`notifications:${call.params[0]}`, 0, -1);
+    console.log('Send notifications', call.params[0], res.length);
+    const notifications = res.map((notification) => JSON.parse(notification));
+    ws.send(JSON.stringify({ id: call.id, result: notifications }));
+  } catch (error) {
+    console.log('Redis get_notifications failed', error);
+  }
+};
+
 class WebSocket {
   constructor() {
     this.wss = wss;
-    this.wss.on('connection', (ws) => {
+    this.wss.on('connection', async (ws) => {
       console.log('Got connection from new peer');
-      ws.on('message', (message) => {
+      ws.on('message', async (message) => {
         console.log('Message', message);
         let call = {};
         try {
@@ -52,28 +63,20 @@ class WebSocket {
         } catch (e) {
           console.error('Error WS parse JSON message', message, e);
         }
-        // const key = new Buffer(JSON.stringify([call.method, call.params])).toString('base64');
         if (call.method === 'get_notifications' && call.params && call.params[0]) {
-          redisNotifyClient.lrangeAsync(`notifications:${call.params[0]}`, 0, -1).then((res) => {
-            console.log('Send notifications', call.params[0], res.length);
-            const notifications = res.map((notification) => JSON.parse(notification));
-            ws.send(JSON.stringify({ id: call.id, result: notifications }));
-          }).catch((err) => {
-            console.log('Redis get_notifications failed', err);
-          });
+          await getNotifications(call, ws);
         } else if (call.method === 'login' && call.params && call.params[0]) {
           sc2.setAccessToken(call.params[0]);
-          sc2.me().then((result) => {
+          try {
+            const result = await sc2.me();
             sendLoginSuccess(call, result, ws);
-          }).catch(() => {
+          } catch (error) {
             sendSomethingWrong(call, ws);
-          });
+          }
         } else if (call.method === 'guest_login' && call.params && call.params[0]) {
-          validateAuthToken(call.params[0]).then(({ result }) => {
-            sendLoginSuccess(call, result, ws);
-          }).catch(() => {
-            sendSomethingWrong(call, ws);
-          });
+          const { result } = await validateAuthToken(call.params[0]);
+          if (result) sendLoginSuccess(call, result, ws);
+          else sendSomethingWrong(call, ws);
         } else if (call.method === 'subscribe' && call.params && call.params[0]) {
           console.log('Subscribe success', call.params[0]);
           ws.name = call.params[0];
@@ -81,15 +84,15 @@ class WebSocket {
             { id: call.id, result: { subscribe: true, username: call.params[0] } },
           ));
         } else if (call.method === 'unsubscribe' && call.params && call.params[0]) {
-          sc2.setAccessToken(call.params[0]);
-          sc2.me()
-            .then((result) => unSubscribe(call, result, ws))
-            .catch(() => {
-              validateAuthToken(call.params[0]).then(({ result }) => {
-                if (result) unSubscribe(call, result, ws);
-              });
-              sendSomethingWrong(call, ws);
-            });
+          try {
+            sc2.setAccessToken(call.params[0]);
+            const result = await sc2.me();
+            unSubscribe(call, result, ws);
+          } catch (error) {
+            const { result } = await validateAuthToken(call.params[0]);
+            if (result) unSubscribe(call, result, ws);
+            else sendSomethingWrong(call, ws);
+          }
         } else {
           sendSomethingWrong(call, ws);
         }
