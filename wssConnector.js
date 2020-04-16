554e3087b8de3c21970c79ea23728ae95fb7f6f9
olegtsigulov <olegcigulev@gmail.com>
2020-04-16 16:29:06 +0300

    change ws logic

--- wssConnector.js
+++ wssConnector.js
@@ -7,15 +7,37 @@ const { validateAuthToken } = require('./helpers/waivioAuthHelper');
 const sc2 = sdk.Initialize({ app: 'waivio.app' });
 const wss = new SocketServer({ server, path: '/notifications-api' });
 
-const clearGC = () => {
-  try {
-    global.gc();
-  } catch (e) {
-    console.log("You must run program with 'node --expose-gc index.js' or 'npm start'");
-  }
+const sendLoginSuccess = (call, result, ws) => {
+  console.log('Login success', result.name);
+  ws.name = result.name;
+  ws.verified = true;
+  ws.account = result.account;
+  ws.user_metadata = result.user_metadata;
+  ws.send(JSON.stringify(
+    { id: call.id, result: { login: true, username: result.name } },
+  ));
 };
 
-setInterval(clearGC, 60 * 1000);
+const sendSomethingWrong = (call, ws) => {
+  ws.send(
+    JSON.stringify({
+      id: call.id,
+      result: {},
+      error: 'Something is wrong',
+    }),
+  );
+};
+
+const unSubscribe = (call, result, ws) => {
+  wss.clients.forEach((client) => {
+    if (client.name === result.name) {
+      wss.clients.delete(client);
+      ws.send(
+        JSON.stringify({ id: call.id, result: { subscribe: false, username: result.name } }),
+      );
+    }
+  });
+};
 
 class WebSocket {
   constructor() {
@@ -32,78 +54,44 @@ class WebSocket {
         }
         // const key = new Buffer(JSON.stringify([call.method, call.params])).toString('base64');
         if (call.method === 'get_notifications' && call.params && call.params[0]) {
-          redisNotifyClient
-            .lrangeAsync(`notifications:${call.params[0]}`, 0, -1)
-            .then((res) => {
-              console.log('Send notifications', call.params[0], res.length);
-              const notifications = res.map((notification) => JSON.parse(notification));
-              ws.send(JSON.stringify({ id: call.id, result: notifications }));
-            })
-            .catch((err) => {
-              console.log('Redis get_notifications failed', err);
-            });
-          // } else if (useCache && cache[key]) {
-          //  ws.send(JSON.stringify({ id: call.id, cache: true, result: cache[key] }));
+          redisNotifyClient.lrangeAsync(`notifications:${call.params[0]}`, 0, -1).then((res) => {
+            console.log('Send notifications', call.params[0], res.length);
+            const notifications = res.map((notification) => JSON.parse(notification));
+            ws.send(JSON.stringify({ id: call.id, result: notifications }));
+          }).catch((err) => {
+            console.log('Redis get_notifications failed', err);
+          });
         } else if (call.method === 'login' && call.params && call.params[0]) {
           sc2.setAccessToken(call.params[0]);
-          sc2
-            .me()
-            .then((result) => {
-              console.log('Login success', result.name);
-              ws.name = result.name;
-              ws.verified = true;
-              ws.account = result.account;
-              ws.user_metadata = result.user_metadata;
-              ws.send(JSON.stringify(
-                { id: call.id, result: { login: true, username: result.name } },
-              ));
-            })
-            .catch((err) => {
-              console.error('Login failed', err);
-              ws.send(
-                JSON.stringify({
-                  id: call.id,
-                  result: {},
-                  error: 'Something is wrong',
-                }),
-              );
-            });
+          sc2.me().then((result) => {
+            sendLoginSuccess(call, result, ws);
+          }).catch(() => {
+            sendSomethingWrong(call, ws);
+          });
         } else if (call.method === 'guest_login' && call.params && call.params[0]) {
-          validateAuthToken(call.params[0])
-            .then(({ error, result }) => {
-              console.log('Login success', result.name);
-              ws.name = result.name;
-              ws.verified = true;
-              ws.account = result.account;
-              ws.user_metadata = result.user_metadata;
-              ws.send(JSON.stringify(
-                { id: call.id, result: { login: true, username: result.name } },
-              ));
-            })
-            .catch((err) => {
-              console.error('Login failed', err);
-              ws.send(
-                JSON.stringify({
-                  id: call.id,
-                  result: {},
-                  error: 'Something is wrong',
-                }),
-              );
-            });
+          validateAuthToken(call.params[0]).then(({ result }) => {
+            sendLoginSuccess(call, result, ws);
+          }).catch(() => {
+            sendSomethingWrong(call, ws);
+          });
         } else if (call.method === 'subscribe' && call.params && call.params[0]) {
           console.log('Subscribe success', call.params[0]);
           ws.name = call.params[0];
-          ws.send(
-            JSON.stringify({ id: call.id, result: { subscribe: true, username: call.params[0] } }),
-          );
+          ws.send(JSON.stringify(
+            { id: call.id, result: { subscribe: true, username: call.params[0] } },
+          ));
+        } else if (call.method === 'unsubscribe' && call.params && call.params[0]) {
+          sc2.setAccessToken(call.params[0]);
+          sc2.me()
+            .then((result) => unSubscribe(call, result, ws))
+            .catch(() => {
+              validateAuthToken(call.params[0]).then(({ result }) => {
+                if (result) unSubscribe(call, result, ws);
+              });
+              sendSomethingWrong(call, ws);
+            });
         } else {
-          ws.send(
-            JSON.stringify({
-              id: call.id,
-              result: {},
-              error: 'Something is wrong',
-            }),
-          );
+          sendSomethingWrong(call, ws);
         }
       });
       ws.on('error', () => console.log('Error on connection with peer'));
