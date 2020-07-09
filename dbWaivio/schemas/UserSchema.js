ab6d68d90c6e9573c2dd28217c984469b5341dc6
Boris Andrzhiievskyi <borees2@gmail.com>
2020-07-09 10:38:57 +0300

    add default UserSchema.js

--- dbWaivio/schemas/UserSchema.js
+++ dbWaivio/schemas/UserSchema.js
@@ -36,7 +36,7 @@ const UserMetadataSchema = new Schema({
       type: Number, min: 1, max: 10000, default: 5000,
     }, // default percent of your upvotes
     votingPower: { type: Boolean, default: false }, // dynamic toggle of vote power on each vote
-    userNotifications: { type: UserNotificationsSchema },
+    userNotifications: { type: UserNotificationsSchema, default: () => ({}) },
   },
   bookmarks: { type: [String], default: [] },
   drafts: {
