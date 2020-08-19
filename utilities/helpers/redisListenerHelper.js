503d104622adef632461ba2f0899d1847b19590f
Maxim <maksimoancha12@gmail.com>
2020-08-19 11:43:55 +0300

    add check for parsed block

--- utilities/helpers/redisListenerHelper.js
+++ utilities/helpers/redisListenerHelper.js
@@ -2,10 +2,10 @@ const { redis, redisGetter, redisSetter } = require('../redis');
 const wssHelper = require('./wssHelper');
 
 const messageDataListener = async (channel, msg) => {
-  const subscribers = await redisGetter.getBlockSubscribers(`${channel}:${+msg - 1}`);
+  const subscribers = await redisGetter.getBlockSubscribers(`${channel}:${msg}`);
   if (subscribers && subscribers.length) {
-    await wssHelper.sendParsedBlockResponse(channel, subscribers, +msg - 1);
-    await redisSetter.deleteSubscribers(`${channel}:${+msg - 1}`);
+    await wssHelper.sendParsedBlockResponse(channel, subscribers, msg);
+    await redisSetter.deleteSubscribers(`${channel}:${msg}`);
   }
 };
 
