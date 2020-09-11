3f4083966809267b8bac24955a5987e1471c644a
Boris Andrzhiievskyi <borees2@gmail.com>
2020-09-11 17:06:11 +0300

    change myComment

--- utilities/notificationsParser/comment.js
+++ utilities/notificationsParser/comment.js
@@ -6,6 +6,8 @@ const {
   getUsers, checkUserNotifications, getServiceBots,
   addNotificationForSubscribers,
 } = require('utilities/helpers/notificationsHelper');
+const { App } = require('models');
+const config = require('config');
 
 module.exports = async (params) => {
   const notifications = [];
@@ -59,15 +61,21 @@ module.exports = async (params) => {
       if (await checkUserNotifications(
         { user: _.find(authors, { name: params.author }), type: NOTIFICATIONS_TYPES.MY_COMMENT },
       )) {
+        const { app } = await App.getOne({ condition: { name: config.app } });
+        const serviceBots = _.includes(_.map(app.service_bots, 'name'), params.parent_author);
+        const replyTo = serviceBots ? 'guest user' : params.parent_author;
+
         notification = {
           timestamp: Math.round(new Date().valueOf() / 1000),
           type: NOTIFICATIONS_TYPES.MY_COMMENT,
           permlink: params.permlink,
           author: params.author,
+          parentAuthor: params.parent_author,
+          guestUser: serviceBots,
         };
         notifications.push([params.author, notification]);
 
-        await shareMessageBySubscribers(params.author, `${params.author} write a comment`,
+        await shareMessageBySubscribers(params.author, `${params.author} reply to ${replyTo}`,
           `${PRODUCTION_HOST}@${params.author}/${params.permlink}`);
       }
       break;
