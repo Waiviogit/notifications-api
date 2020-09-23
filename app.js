50b0970821080d7d1d416ab55b4071147f9e7ba2
olegtsigulov <olegcigulev@gmail.com>
2020-09-23 15:48:11 +0300

    add sentry

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Sentry = require('@sentry/node');
+const Tracing = require('@sentry/tracing');
 const morgan = require('morgan');
 const swaggerUi = require('swagger-ui-express');
+const { sendSentryNotification } = require('utilities/helpers/sentryHelper');
 const swaggerDocument = require('./swagger/swagger.json');
 const authMiddleware = require('./middlewares/authMiddleware');
 
@@ -12,15 +15,44 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 4000;
 const app = express();
+
+Sentry.init({
+  environment: process.env.NODE_ENV,
+  dsn: process.env.SENTRY_DNS,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+});
+
+process.on('unhandledRejection', (error) => {
+  sendSentryNotification();
+  Sentry.captureException(error);
+});
+
 exports.server = app.listen(port, () => console.log(`Listening on ${port}`));
 
 const router = require('./routes');
 
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+
+app.use(Sentry.Handlers.requestHandler({ request: true, user: true }));
 app.use('/', authMiddleware, router);
 app.use('/notifications/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.use(Sentry.Handlers.errorHandler({
+  shouldHandleError(error) {
+    // Capture 500 errors
+    if (error.status >= 500 || !error.status) {
+      sendSentryNotification();
+      return true;
+    }
+    return false;
+  },
+}));
 app.use((req, res, next) => {
   res.status(res.result.status || 200).json(res.result.json);
 });
