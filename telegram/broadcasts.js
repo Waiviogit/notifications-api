266a35ac93452c7c082ee431bf48e1ef72eeabc2
Boris Andrzhiievskyi <borees2@gmail.com>
2021-02-24 16:28:27 +0200

    fix telegrams send message

--- telegram/broadcasts.js
+++ telegram/broadcasts.js
@@ -10,10 +10,14 @@ exports.shareMessageBySubscribers = async (user, message, url) => {
   const { notifiers } = await notifiersModel.find({ condition: { subscribedUsers: user } });
   if (!notifiers || !notifiers.length) return;
   for (const notifier of notifiers) {
-    await WaivioBot.bot.sendMessage(notifier.chatId, message, {
-      reply_markup: {
-        inline_keyboard: [[{ text: 'Go to website', url }], [{ text: `Unsubscribe ${user}`, callback_data: `unsubscribe:${user}` }]],
-      },
-    });
+    try {
+      await WaivioBot.bot.sendMessage(notifier.chatId, message, {
+        reply_markup: {
+          inline_keyboard: [[{ text: 'Go to website', url }], [{ text: `Unsubscribe ${user}`, callback_data: `unsubscribe:${user}` }]],
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
   }
 };
