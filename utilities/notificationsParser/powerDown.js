07d94fe3105fad300ff4d8b7ec8d86879e61fb9e
unknown <Alishern@wizardsdev@gmail.com>
2022-02-09 15:22:38 +0600

    change format of data

--- utilities/notificationsParser/powerDown.js
+++ utilities/notificationsParser/powerDown.js
@@ -3,13 +3,16 @@ const { shareMessageBySubscribers } = require('telegram/broadcasts');
 
 const { NOTIFICATIONS_TYPES } = require('constants/notificationTypes');
 
-module.exports = async (operation, params) => {
+module.exports = async (params) => {
   await shareMessageBySubscribers(params.from,
     `${params.from} initiated 'Power Down' on ${params.amount}`,
-    `${PRODUCTION_HOST}@${params.account}`);
+    `${PRODUCTION_HOST}@${params.from}`);
 
-  return [params.from, Object.assign(params, {
+  return [params.from, {
     timestamp: Math.round(new Date().valueOf() / 1000),
-    type: NOTIFICATIONS_TYPES.WITHDRAW_VESTING,
-  })];
+    type: NOTIFICATIONS_TYPES.POWER_DOWN,
+    account: params.from,
+    block: params.block,
+    amonut: params.amount,
+  }];
 };
