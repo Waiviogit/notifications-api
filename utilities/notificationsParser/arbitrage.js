73321049c9155640d6735134e94f809eaa7cb5ad
MashaYakovenko <mariiay@wizardsdev.com>
2022-07-14 11:24:22 +0300

    user notifications on arbitrage

--- utilities/notificationsParser/arbitrage.js
+++ utilities/notificationsParser/arbitrage.js
@@ -9,11 +9,11 @@ module.exports = async (params) => {
     const message = `The profitable difference percent on pair ${el.tokenPair} is ${el.differencePercent}% now.
       Follow the link ${link} to proceed to the swap transactions.`;
     await shareMessageBySubscribers(el.account, message, link);
-    notifications.push({
+    notifications.push([el.account, {
       timestamp: Math.round(new Date().valueOf() / 1000),
       type: NOTIFICATIONS_TYPES.ARBITRAGE,
       message,
-    });
+    }]);
   }
 
   return notifications;
