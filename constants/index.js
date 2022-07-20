26a09b946d665dfe8957ec0358110266ebbdb93b
MashaYakovenko <mariiay@wizardsdev.com>
2022-07-20 17:11:21 +0300

    word new removed from rebalancing url

--- constants/index.js
+++ constants/index.js
@@ -2,4 +2,4 @@ exports.NOTIFICATION_EXPIRY = 5 * 24 * 3600;
 exports.LIMIT = 25;
 
 exports.PRODUCTION_HOST = 'https://www.waivio.com/';
-exports.REBALANCING = 'rewards-new/rebalancing?username=';
+exports.REBALANCING = 'rewards/rebalancing?username=';
