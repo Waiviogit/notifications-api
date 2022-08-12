666a3fd2ae25f7711457f53d140d7987cf789fd7
MashaYakovenko <mariiay@wizardsdev.com>
2022-08-12 11:17:12 +0300

    power up fix

--- utilities/notificationsParser/powerUp.js
+++ utilities/notificationsParser/powerUp.js
@@ -4,32 +4,52 @@ const { NOTIFICATIONS_TYPES } = require('constants/notificationTypes');
 const { checkUserNotifications, getUsers } = require('utilities/helpers/notificationsHelper');
 
 module.exports = async (params) => {
-  const { user, error } = await getUsers({ single: params.from });
-
-  if (error) {
-    console.error(error.message);
-    return [];
-  }
-  if (!await checkUserNotifications({ user, type: 'powerUp' })) return [];
-
+  const notifications = [];
   const payload = Object.assign(params, {
     timestamp: Math.round(new Date().valueOf() / 1000),
     type: NOTIFICATIONS_TYPES.TRANSFER_TO_VESTING,
   });
-  if (params.from === params.to) {
-    await shareMessageBySubscribers(params.from,
-      `${params.from} initiated 'Power Up' on ${params.amount} `,
-      `${PRODUCTION_HOST}@${params.from}/transfers`);
+  if (params.from === params.to) notifications.push(await getUserNotifications(params, payload));
+  else notifications.push(...await getUsersNotifications(params, payload));
 
-    return [params.from, payload];
+  return notifications;
+};
+
+const getUserNotifications = async (params, payload) => {
+  const { user, error } = await getUsers({ single: params.from });
+
+  if (error) {
+    console.error(error.message);
+
+    return [];
   }
 
-  const message = `${params.from} initiated 'Power Up' on ${params.amount} to ${params.to}`;
-  let url = `${PRODUCTION_HOST}@${params.from}/transfers`;
-  await shareMessageBySubscribers(params.from, message, url);
-  url = `${PRODUCTION_HOST}@${params.to}/transfers`;
-  await shareMessageBySubscribers(params.to, message, url);
-  const notifications = [[params.from, payload], [params.to, payload]];
+  if (!await checkUserNotifications({ user, type: 'powerUp' })) return [];
+
+  await shareMessageBySubscribers(params.from,
+    `${params.from} initiated 'Power Up' on ${params.amount} `,
+    `${PRODUCTION_HOST}@${params.from}/transfers`);
+
+  return [params.from, payload];
+};
+
+const getUsersNotifications = async (params, payload) => {
+  const notifications = [];
+  const { users, error } = await getUsers({ arr: [params.from, params.to] });
+
+  if (error) {
+    console.error(error.message);
+
+    return [];
+  }
+  for (const user of users) {
+    if (!await checkUserNotifications({ user, type: 'powerUp' })) break;
+
+    await shareMessageBySubscribers(user.name,
+      `${params.from} initiated 'Power Up' on ${params.amount} to ${params.to}`,
+      `${PRODUCTION_HOST}@${user.name}/transfers`);
+    notifications.push([user.name, payload]);
+  }
 
   return notifications;
 };
